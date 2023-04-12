120a822e47484296ef0fa02b5a77592bcc0643cf
migue-ush <albornozmiguel@gmail.com>
2023-04-12 11:16:03 -0300

    actualización archivo app.js con rutas register/login

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const express = require("express");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -8,7 +10,15 @@ app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
   })
 
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/home.html")
-    
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");  
 });
